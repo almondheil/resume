585b2f37c79db18d32521d692fa8c198a1a9082e
Almond Heil <almond@almendra.dev>
2023-09-27 16:59:45 -0500

    Don't capitalize name

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 TODAY=$(date +%Y-%m-%d)
 
 # Archive current tex file to hold onto old versions in future
-cp "AlmondHeil-Resume.tex" "archive/archived-$TODAY.tex"
+cp "almondheil-Resume.tex" "archive/archived-$TODAY.tex"
 
 # Compile tex file
-pdflatex -output-directory="aux" AlmondHeil-Resume.tex
+pdflatex -output-directory="aux" almondheil-Resume.tex
 
 # Move the pdf to top level so it's easier to access
-mv aux/AlmondHeil-Resume.pdf .
+mv aux/almondheil-Resume.pdf .
